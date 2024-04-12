5e075a73abadcfdd0304f32956e5938130936fb5
+ <+>
2024-04-12 14:33:07 -0700

    fixed donation form

--- frontend/src/DonationForm.js
+++ frontend/src/DonationForm.js
@@ -3,21 +3,21 @@ import React from "react";
 function DonationForm(){
     function handleSubmit(e){
         e.preventDefault();
-        fetch('http://localhost:3000/albums', {
+        fetch('http://localhost:3000/books', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 title: e.target.title.value,
-                artist: e.target.artist.value,
-                length: e.target.length.value,
+                author: e.target.author.value,
+                genre: e.target.genre.value,
                 image: e.target.image.value,
                 inStock: 1
             })
-            .then(()=>{
-                console.log('Thank you for your donation!')
-            })
+        })
+        .then(()=>{
+            console.log('Thank you for your donation!')
         })
     }
     return (
